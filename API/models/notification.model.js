6990f9b430318c3d61b128296f3e84235e6ddf05
ancedric <101697896+ancedric@users.noreply.github.com>
2025-09-16 16:53:42 +0100

    Update notification.model.js

--- API/models/notification.model.js
+++ API/models/notification.model.js
@@ -2,7 +2,7 @@ import db from '../services/db.js';
 
 export const createNotification = async (notifRef, title, content, userRef) => {
     const sql = `
-        INSERT INTO notification (notifRef, title, content, userRef, createdAt, isRead) 
+        INSERT INTO s (notifRef, title, content, userRef, createdAt, isRead) 
         VALUES ($1, $2, $3, $4, NOW(), FALSE) RETURNING *;
     `;
     const result = await db.query(sql, [notifRef, title, content, userRef]);
@@ -10,31 +10,31 @@ export const createNotification = async (notifRef, title, content, userRef) => {
 };
 
 export const getAllNotifications = async () => {
-    const sql = `SELECT * FROM notification ORDER BY createdAt DESC;`;
+    const sql = `SELECT * FROM notifications ORDER BY createdAt DESC;`;
     const result = await db.query(sql);
     return result.rows;
 };
 
 export const getNotificationsByUser = async (userRef) => {
-    const sql = `SELECT * FROM notification WHERE userRef = $1 ORDER BY createdAt DESC;`;
+    const sql = `SELECT * FROM notifications WHERE userRef = $1 ORDER BY createdAt DESC;`;
     const result = await db.query(sql, [userRef]);
     return result.rows;
 };
 
 export const getNotificationByRef = async (notifRef) => {
-    const sql = `SELECT * FROM notification WHERE notifRef = $1;`;
+    const sql = `SELECT * FROM notifications WHERE notifRef = $1;`;
     const result = await db.query(sql, [notifRef]);
     return result.rows[0];
 };
 
 export const updateNotificationStatus = async (notifRef, isRead) => {
-    const sql = `UPDATE notification SET isRead = $1 WHERE notifRef = $2 RETURNING *;`;
+    const sql = `UPDATE notifications SET isRead = $1 WHERE notifRef = $2 RETURNING *;`;
     const result = await db.query(sql, [isRead, notifRef]);
     return result.rows[0];
 };
 
 export const deleteNotification = async (notifRef) => {
-    const sql = `DELETE FROM notification WHERE notifRef = $1;`;
+    const sql = `DELETE FROM notifications WHERE notifRef = $1;`;
     const result = await db.query(sql, [notifRef]);
     return result.rowCount;
 };
